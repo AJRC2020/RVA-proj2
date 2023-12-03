aca80417939d38ba526e3fa9967ecb3944f0e757
AJRC2020 <ajrc2020@gmail.com>
2023-12-03 14:59:25 +0000

    Added capture UI to Insect

--- Assets/Scripts/CaptureInsectController.cs
+++ Assets/Scripts/CaptureInsectController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CaptureInsectController : MonoBehaviour
 {
@@ -10,7 +11,13 @@ public class CaptureInsectController : MonoBehaviour
 
     public Transform camera;
 
+    public Image progressBar;
+    public Image checkmark;
+    public Image cross;
+
     private AudioClip waveAudio;
+    private bool isCaptured = false;
+    private float soundPercentage = 0.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -21,15 +28,27 @@ public class CaptureInsectController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!isCaptured)
+        {
+            Capture();
+            UpdateUI();
+        }
+    }
+
+    void Capture()
+    {
         if (camera.position.z < behindDistance)
         {
             Debug.Log("Behind");
 
             float volume = GetVolume(Microphone.GetPosition(Microphone.devices[0]), waveAudio);
 
+            soundPercentage = Mathf.Clamp01(volume / volumeThreshold);
+
             if (volume > volumeThreshold)
             {
                 Debug.Log("Captured Volume = " + volume);
+                isCaptured = true;
             }
         }
     }
@@ -60,5 +79,22 @@ public class CaptureInsectController : MonoBehaviour
         }
 
         return volumeTotal;
+    }
+
+    void UpdateUI()
+    {
+        if (camera.position.z < behindDistance)
+        {
+            checkmark.enabled = true;
+            cross.enabled = false;
+            progressBar.fillAmount = soundPercentage;
+            progressBar.color = Color.Lerp(Color.green, Color.red, soundPercentage);
+        }
+        else
+        {
+            checkmark.enabled = false;
+            cross.enabled = true;
+            progressBar.fillAmount = 0.0f;
+        }
     }
 }
