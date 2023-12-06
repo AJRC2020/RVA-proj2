6b25a89989593e6fb90a2ec83da52c71eb75dc2a
AJRC2020 <72152717+AJRC2020@users.noreply.github.com>
2023-12-06 10:32:18 +0000

    Change rhino catching method

--- Assets/Scripts/CaptureRhinoController.cs
+++ Assets/Scripts/CaptureRhinoController.cs
@@ -1,28 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using Vuforia;
 
 public class CaptureRhinoController : MonoBehaviour
 {
-    public float luminosityThreshold = 0.2f;
-    public float blackThreshold = 0.1f;
-    public RawImage cameraFeed;
+    public GameObject projectilePrefab;
+    public float arrowSpeed = 10.0f;
     public UnityEngine.UI.Image progressBar;
+    public float arrowDamage = 0.2f;
 
     private Camera cam;
-    private RenderTexture rt;
     private bool isCaptured = false;
-    private float luminosityPercentage = 0.0f;
+    private float hp = 1;
 
     // Start is called before the first frame update
     void Start()
     {
         cam = Camera.main;
-        rt = new RenderTexture(Screen.width, Screen.height, 24);
-        cam.targetTexture = rt;
-        cameraFeed.texture = rt;
     }
 
     // Update is called once per frame
@@ -30,75 +27,33 @@ public class CaptureRhinoController : MonoBehaviour
     {
         if (!isCaptured)
         {
-            CaptureLogic();
             updateUI();
         }
     }
 
-    private void CaptureLogic()
+    public void CreateArrow()
     {
-        cam.Render();
-
-        Texture2D tex = new Texture2D(Screen.width, Screen.height, TextureFormat.RGBA32, false);
-        RenderTexture.active = rt;
-        tex.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
-        tex.Apply();
-
-        cameraFeed.texture = tex;
-
-        /*float luminosityLevel = GetLuminosityLevels(tex.GetPixels());
-
-        if (luminosityLevel < luminosityThreshold)
+        if (!isCaptured)
         {
-            luminosityLevel = luminosityThreshold;
+            GameObject arrow = Instantiate(projectilePrefab, cam.transform.position, Quaternion.identity);
+
+            Vector3 dir = cam.transform.forward;
+
+            arrow.GetComponent<Rigidbody>().AddForce(dir * arrowSpeed);
         }
-
-        luminosityPercentage = (1 / (luminosityThreshold - 1)) * luminosityLevel - (1 / (luminosityThreshold - 1));
-
-        if (luminosityPercentage == 1)
-        {
-            isCaptured = true;
-        }
-
-        Debug.Log("Luminosity Percentage = " + luminosityPercentage * 100 + "%    Luminosity Level = " + luminosityLevel);*/
-    }
-
-    private float GetLuminosityLevels(Color[] pixels)
-    {
-        float totalLuminosity = 0;
-        int pixelCount = 0;
-
-        foreach (Color pixel in pixels)
-        {
-            if (pixel.grayscale > blackThreshold)
-            {
-                float linearLuminosity = pixel.grayscale;
-
-                totalLuminosity += linearLuminosity;
-                pixelCount++;
-            }
-        }
-
-        float averagedLuminosity = totalLuminosity / pixelCount;
-
-        return averagedLuminosity;
     }
 
     private void updateUI()
     {
-        progressBar.fillAmount = luminosityPercentage;
-        progressBar.color = Color.Lerp(Color.yellow, Color.black, luminosityPercentage);
+        progressBar.fillAmount = hp;
     }
 
-    public void Disable()
+    private void OnTriggerEnter(Collider other)
     {
-        cam.targetTexture = null;
-        cameraFeed.texture = null;
-    }
-
-    public void Enable()
-    {
-        cam.targetTexture = rt;
-        cameraFeed.texture = rt;
+        if (other.gameObject.layer == 6)
+        {
+            hp -= arrowDamage;
+            Destroy(other.gameObject);
+        }
     }
 }
--- Assets/Scripts/TargetController.cs
+++ Assets/Scripts/TargetController.cs
@@ -7,42 +7,22 @@ using UnityEngine.UI;
 public class TargetController : MonoBehaviour
 {
     public TextMeshProUGUI message;
-    public RawImage raw;
     public GameObject monsterUI;
     public GameObject monster;
 
     public void EnableMessage()
     {
         message.enabled = true;
-        raw.enabled = false;
     }
 
     public void DisableMessage()
     {
         message.enabled = false;
-        raw.enabled = false;
-    }
-
-    public void EnableRhinoUI()
-    {
-        message.enabled = false;
-        raw.enabled = true;
-        monsterUI.SetActive(true);
-        monster.SetActive(true);
-    }
-
-    public void DisableRhinoUI()
-    {
-        message.enabled = true;
-        raw.enabled = false;
-        monster.SetActive(false);
-        monsterUI.SetActive(false);
     }
 
     public void EnableGeneric()
     {
         message.enabled = false;
-        raw.enabled = false;
         monsterUI.SetActive(true);
         monster.SetActive(true);
     }
@@ -50,7 +30,6 @@ public class TargetController : MonoBehaviour
     public void DisableGeneric()
     {
         message.enabled = true;
-        raw.enabled = false;
         monsterUI.SetActive(false);
         monster.SetActive(false);
     }
