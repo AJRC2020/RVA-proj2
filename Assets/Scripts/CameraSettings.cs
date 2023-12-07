f289e2269b40c438e85ccc085bb22026986fe0fa
Joana Mesquita <up201907878@fe.up.pt>
2023-12-07 14:46:52 +0000

    Add camera config spcipt

---
+++ Assets/Scripts/CameraSettings.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+
+public class CameraSettings : MonoBehaviour
+{
+
+    void Start()
+    {
+        VuforiaApplication.Instance.OnVuforiaStarted += OnVuforiaStarted;
+    }
+
+    private void OnVuforiaStarted()
+    {
+
+        VuforiaBehaviour.Instance.CameraDevice.SetCameraMode(Vuforia.CameraMode.MODE_DEFAULT);
+        bool focusModeSet = VuforiaBehaviour.Instance.CameraDevice.SetFocusMode(FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+        if (!focusModeSet)
+        {
+            Debug.Log("Failed to set focus mode" + focusModeSet);
+        }
+        var exposureMode = ExposureMode.EXPOSURE_MODE_CONTINUOUSAUTO;
+
+        if (VuforiaBehaviour.Instance.CameraDevice.IsExposureModeSupported(exposureMode))
+        {
+            VuforiaBehaviour.Instance.CameraDevice.SetExposureMode(ExposureMode.EXPOSURE_MODE_CONTINUOUSAUTO);
+
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
