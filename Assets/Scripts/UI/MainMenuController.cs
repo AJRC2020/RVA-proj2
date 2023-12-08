ce9c0935305b0ee28ceb1cf33df7ffe46448707b
Joana Mesquita <up201907878@fe.up.pt>
2023-12-08 19:29:06 +0000

    Change app icon and add main menu

---
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+
+    public GameObject credits;
+    public GameObject instructions;
+
+
+    public void Play()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void HowToPlay()
+    {
+        instructions.SetActive(true);
+        this.GameObject().SetActive(false);
+    }
+
+    public void Credits()
+    {
+        credits.SetActive(true);
+        this.GameObject().SetActive(false);
+    }
+    
+}
